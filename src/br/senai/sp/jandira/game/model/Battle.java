c4575b827053c0d3b81983914f610cdcad5494e2
NPS <nicolaspetri18@gmail.com>
2023-08-23 11:01:19 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/br/senai/sp/jandira/game/model/Battle.java
+++ src/br/senai/sp/jandira/game/model/Battle.java
@@ -1,4 +1,23 @@
 package br.senai.sp.jandira.game.model;
 
+import java.util.Scanner;
+
 public class Battle {
+
+    /* Instancia Scanner*/
+    Scanner teclado = new Scanner(System.in);
+
+    /* Instancia Scenario*/
+    Scenary scenary = new Scenary();
+    public void ChoiceScenary(){
+        System.out.println("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/");
+        System.out.println("*****************************************************");
+        System.out.print("Antes de dar inico à batalha, \n selecione o cenário: ");
+        System.out.println("[ 1 ] Torre Eifell \n [ 2 ] Arabia {Extra Neymar} \n [ 3 ] Turquia");
+        System.out.println("*****************************************************");
+        System.out.println("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/");
+
+        int choiceScenario = teclado.nextInt();
+    }
+
 }
--- src/br/senai/sp/jandira/game/model/Scenary.java
+++ src/br/senai/sp/jandira/game/model/Scenary.java
@@ -1,4 +1,22 @@
 package br.senai.sp.jandira.game.model;
 
 public class Scenary {
+
+    public String Scenario(int optionScenario){
+
+        String cenario = "Senai Jandira";
+
+        switch (optionScenario){
+            case 1:
+                cenario = "Torre Eiffel";
+                break;
+            case 2:
+                cenario = "Arabia - [Extra Menino Ney]";
+                break;
+            case 3:
+                cenario = "Turquia";
+                break;
+        }
+        return cenario;
+    }
 }
